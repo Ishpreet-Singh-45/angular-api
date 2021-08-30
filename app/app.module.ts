97a8111cb1b1f3b50cd868bd05a7b801c891ec76
Ishpreet-Singh-45 <87083387+Ishpreet-Singh-45@users.noreply.github.com>
2021-08-31 05:03:34 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+	declarations: [
+		AppComponent,
+		RegisterComponent,
+		LoginComponent,
+		HomeComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		ReactiveFormsModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
+})
+export class AppModule { }
